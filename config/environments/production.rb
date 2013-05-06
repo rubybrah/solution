3283dc83922c24a992da19fec9de197d86aa6f0a
JamesDullaghan <james@fldlvl.com>
2013-05-05 19:03:32 -0700

    Add mailer address to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ JamesPortfolio::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Devise Action Mailer Url
+  config.action_mailer.default_url_options = { :host => 'http://still-springs-3519.herokuapp.com' }
 end
